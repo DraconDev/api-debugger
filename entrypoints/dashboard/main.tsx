ba366f60faf08748eb63000c40c440b24bc94c29
DraconDev <dracsharp@gmail.com>
2026-03-12 06:49:52 +0000

    [[daemon] sync: 3 file(s)|wip] 3 modified entrypoints/dashboard/main.tsx package-lock.json package.json

--- entrypoints/dashboard/main.tsx
+++ entrypoints/dashboard/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Dashboard from "./Dashboard";
+import "./style.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
