77389b5bea98717a2705478c85463288391985b6
DraconDev <dracsharp@gmail.com>
2026-03-14 09:47:08 +0000

    [[daemon] sync: 3 file(s)|wip] 3 modified components/request/index.ts package-lock.json package.json

--- components/request/index.ts
+++ components/request/index.ts
@@ -3,7 +3,7 @@ export { HeadersEditor } from "./HeadersEditor";
 export { BodyEditor } from "./BodyEditor";
 export { AuthEditor } from "./AuthEditor";
 export { CodeGenerator } from "./CodeGenerator";
-export { ResponseViewer, JsonViewer } from "./ResponseViewer";
+export { ResponseViewer } from "./ResponseViewer";
 export { TimingBreakdown } from "./TimingBreakdown";
 export { VariableExtractor } from "./VariableExtractor";
 export { AIAnalysisPanel } from "./AIAnalysisPanel";
